2ad23c952f32f6038cc4b438ea730413dae1c049
Ciantic <jari.pennanen@gmail.com>
2011-05-13 21:55:51 +0300

    Easier to spot errors in this insanity

--- NppyPluginDll.cpp
+++ NppyPluginDll.cpp
@@ -114,72 +114,40 @@ vector<FuncItem> funcItems;
 // 64 oughta be enough for everybody!
 #define MAX_FUNCS 64
 vector<wstring> funcStatements;
-void func0() { runPython(funcStatements[0]); }
-void func1() { runPython(funcStatements[1]); }
-void func2() { runPython(funcStatements[2]); }
-void func3() { runPython(funcStatements[3]); }
-void func4() { runPython(funcStatements[4]); }
-void func5() { runPython(funcStatements[5]); }
-void func6() { runPython(funcStatements[6]); }
-void func7() { runPython(funcStatements[7]); }
-void func8() { runPython(funcStatements[8]); }
-void func9() { runPython(funcStatements[9]); }
-void func10() { runPython(funcStatements[10]); }
-void func11() { runPython(funcStatements[11]); }
-void func12() { runPython(funcStatements[12]); }
-void func13() { runPython(funcStatements[13]); }
-void func14() { runPython(funcStatements[14]); }
-void func15() { runPython(funcStatements[15]); }
-void func16() { runPython(funcStatements[16]); }
-void func17() { runPython(funcStatements[17]); }
-void func18() { runPython(funcStatements[18]); }
-void func19() { runPython(funcStatements[19]); }
-void func20() { runPython(funcStatements[20]); }
-void func21() { runPython(funcStatements[21]); }
-void func22() { runPython(funcStatements[22]); }
-void func23() { runPython(funcStatements[23]); }
-void func24() { runPython(funcStatements[24]); }
-void func25() { runPython(funcStatements[25]); }
-void func26() { runPython(funcStatements[26]); }
-void func27() { runPython(funcStatements[27]); }
-void func28() { runPython(funcStatements[28]); }
-void func29() { runPython(funcStatements[29]); }
-void func30() { runPython(funcStatements[30]); }
-void func31() { runPython(funcStatements[31]); }
-void func32() { runPython(funcStatements[32]); }
-void func33() { runPython(funcStatements[33]); }
-void func34() { runPython(funcStatements[34]); }
-void func35() { runPython(funcStatements[35]); }
-void func36() { runPython(funcStatements[36]); }
-void func37() { runPython(funcStatements[37]); }
-void func38() { runPython(funcStatements[38]); }
-void func39() { runPython(funcStatements[39]); }
-void func40() { runPython(funcStatements[40]); }
-void func41() { runPython(funcStatements[41]); }
-void func42() { runPython(funcStatements[42]); }
-void func43() { runPython(funcStatements[43]); }
-void func44() { runPython(funcStatements[44]); }
-void func45() { runPython(funcStatements[45]); }
-void func46() { runPython(funcStatements[46]); }
-void func47() { runPython(funcStatements[47]); }
-void func48() { runPython(funcStatements[48]); }
-void func49() { runPython(funcStatements[49]); }
-void func50() { runPython(funcStatements[50]); }
-void func51() { runPython(funcStatements[51]); }
-void func52() { runPython(funcStatements[52]); }
-void func53() { runPython(funcStatements[53]); }
-void func54() { runPython(funcStatements[54]); }
-void func55() { runPython(funcStatements[55]); }
-void func56() { runPython(funcStatements[56]); }
-void func57() { runPython(funcStatements[57]); }
-void func58() { runPython(funcStatements[58]); }
-void func59() { runPython(funcStatements[59]); }
-void func60() { runPython(funcStatements[60]); }
-void func61() { runPython(funcStatements[61]); }
-void func62() { runPython(funcStatements[62]); }
-void func63() { runPython(funcStatements[63]); }
+void  func0() { runPython(funcStatements[ 0]); } void func32() { runPython(funcStatements[32]); }
+void  func1() { runPython(funcStatements[ 1]); } void func33() { runPython(funcStatements[33]); }
+void  func2() { runPython(funcStatements[ 2]); } void func34() { runPython(funcStatements[34]); }
+void  func3() { runPython(funcStatements[ 3]); } void func35() { runPython(funcStatements[35]); }
+void  func4() { runPython(funcStatements[ 4]); } void func36() { runPython(funcStatements[36]); }
+void  func5() { runPython(funcStatements[ 5]); } void func37() { runPython(funcStatements[37]); }
+void  func6() { runPython(funcStatements[ 6]); } void func38() { runPython(funcStatements[38]); }
+void  func7() { runPython(funcStatements[ 7]); } void func39() { runPython(funcStatements[39]); }
+void  func8() { runPython(funcStatements[ 8]); } void func40() { runPython(funcStatements[40]); }
+void  func9() { runPython(funcStatements[ 9]); } void func41() { runPython(funcStatements[41]); }
+void func10() { runPython(funcStatements[10]); } void func42() { runPython(funcStatements[42]); }
+void func11() { runPython(funcStatements[11]); } void func43() { runPython(funcStatements[43]); }
+void func12() { runPython(funcStatements[12]); } void func44() { runPython(funcStatements[44]); }
+void func13() { runPython(funcStatements[13]); } void func45() { runPython(funcStatements[45]); }
+void func14() { runPython(funcStatements[14]); } void func46() { runPython(funcStatements[46]); }
+void func15() { runPython(funcStatements[15]); } void func47() { runPython(funcStatements[47]); }
+void func16() { runPython(funcStatements[16]); } void func48() { runPython(funcStatements[48]); }
+void func17() { runPython(funcStatements[17]); } void func49() { runPython(funcStatements[49]); }
+void func18() { runPython(funcStatements[18]); } void func50() { runPython(funcStatements[50]); }
+void func19() { runPython(funcStatements[19]); } void func51() { runPython(funcStatements[51]); }
+void func20() { runPython(funcStatements[20]); } void func52() { runPython(funcStatements[52]); }
+void func21() { runPython(funcStatements[21]); } void func53() { runPython(funcStatements[53]); }
+void func22() { runPython(funcStatements[22]); } void func54() { runPython(funcStatements[54]); }
+void func23() { runPython(funcStatements[23]); } void func55() { runPython(funcStatements[55]); }
+void func24() { runPython(funcStatements[24]); } void func56() { runPython(funcStatements[56]); }
+void func25() { runPython(funcStatements[25]); } void func57() { runPython(funcStatements[57]); }
+void func26() { runPython(funcStatements[26]); } void func58() { runPython(funcStatements[58]); }
+void func27() { runPython(funcStatements[27]); } void func59() { runPython(funcStatements[59]); }
+void func28() { runPython(funcStatements[28]); } void func60() { runPython(funcStatements[60]); }
+void func29() { runPython(funcStatements[29]); } void func61() { runPython(funcStatements[61]); }
+void func30() { runPython(funcStatements[30]); } void func62() { runPython(funcStatements[62]); }
+void func31() { runPython(funcStatements[31]); } void func63() { runPython(funcStatements[63]); }
 PFUNCPLUGINCMD funcs[] = { 
-	func0, func1, func2, func3, func4, func5, func6, func7, func8, func9, func10, func11, func12, func13, func14, func15, func16, func17, func18, func19, func20, func21, func22, func23, func24, func25, func26, func27, func28, func29, func30, func31,
+	 func0,  func1,  func2,  func3,  func4,  func5,  func6,  func7,  func8,  func9, func10, func11, func12, func13, func14, func15, func16, func17, func18, func19, func20, func21, func22, func23, func24, func25, func26, func27, func28, func29, func30, func31,
 	func32, func33, func34, func35, func36, func37, func38, func39, func40, func41, func42, func43, func44, func45, func46, func47, func48, func49, func50, func51, func52, func53, func54, func55, func56, func57, func58, func59, func60, func61, func62, func63
 };
 
